ea228eea15623737d994d0c70e51a018084c9a77
nguyenquangnam1988vt-eng <nguyenquangnam1988vt@gmail.com>
2026-01-25 13:47:33 +0700

    Update Runner-Bridging-Header.h

--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,1 +1,7 @@
 #import "GeneratedPluginRegistrant.h"
+#import "GeneratedPluginRegistrant.h"
+#import <CoreLocation/CoreLocation.h>
+#import <CoreMotion/CoreMotion.h>
+#import <CoreTelephony/CTCallCenter.h>
+#import <AVFoundation/AVFoundation.h>
+#import <Network/Network.h>
